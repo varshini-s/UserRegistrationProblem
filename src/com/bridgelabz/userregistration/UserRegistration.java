d8374c4a152aefdad2d0c820f2cc7b8f8dd56643
varshini <dreamvarshini@gmail.com>
2021-09-07 19:13:10 +0530

    ADDED: welcome message added

---
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistration 
+{
+
+	
+	public static void main(String[] args) 
+	
+	{
+		
+		System.out.println("welcome to user Registration problem");
+	}
+}
