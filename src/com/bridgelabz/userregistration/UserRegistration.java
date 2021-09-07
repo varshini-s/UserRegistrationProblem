28971ff350166a68e8240c70783a1e2e09f999bd
varshini <dreamvarshini@gmail.com>
2021-09-07 19:17:16 +0530

    ADDED: function to validate first name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,8 +1,59 @@
 package com.bridgelabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration 
 {
 
+	static String firstNamePattern="^[A-Z][a-z]{2,}";
+	
+	String firstName;
+	
+	public  void validation(String fieldToValidate)
+	{
+		String patternToCompile="";
+
+		switch (fieldToValidate) 
+		{
+		case "firstName":
+			patternToCompile=firstNamePattern;
+			break;
+
+		
+
+		}
+
+		String validString="";
+		while(true)
+		{
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("enter valid "+fieldToValidate);
+			validString=scanner.nextLine();
+			Pattern pattern = Pattern.compile(patternToCompile);
+			Matcher matcher = pattern.matcher(validString);
+			boolean matchFound = matcher.find();
+			if(matchFound)
+			{
+				break;
+			}
+
+
+		}
+
+		switch (fieldToValidate) 
+		{
+		case "firstName":
+			firstName=validString;
+			break;
+
+		
+
+		}
+
+	}
 	
 	public static void main(String[] args) 
 	
